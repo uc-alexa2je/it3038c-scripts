a4a7bf4594e5641725103a9da02a0899d89b103b
us-alexa2je <alex2je@mail.uc.edu>
2021-11-08 01:16:32 -0500

    Calculator program

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -26,24 +26,8 @@ print("Your guess is " + myGuess)
 #Pause after showing guessed number
 time.sleep(4)
 
-#begin eval
-if int(myGuess) < value:
-    print("You are too low!")
 
-elif int(myGuess) == value:
-    print("You are correct! The number is: " + value)
 
-elif int(myGuess) > value and int(myGuess) <= 100:
-    print("You are too high!  Try again.")
-
-elif int(myGuess) > 100:
-    print("You are out of range.  Please select between 0 - 100.")
-
-#End the game
-while True:
-    print("Would you like to play again?")
-    reply = input()
-    
 
         
     if reply == no_type_check or reply == no_type_check:
---
+++ python/calculator.py
@@ -0,0 +1,79 @@
+import time
+
+print("What is your name?")
+yName = input()
+
+print("Hello " + yName)
+
+time.sleep(4)
+
+def start():
+    print("Welcome!")
+    time.sleep(2)
+    print("This is a simple calculator program")
+    time.sleep(2)
+    print("Are you ready to begin?")
+    time.sleep(4)
+
+    begin = input("""Please enter Y or N""")
+    if begin.upper() == 'Y':
+       calculator()
+    elif begin.upper() == 'N':
+       print("Goodbye!  Have a nice day! ")
+    else:
+        start()
+    
+    def calculator():
+        calculation = input("""Please enter the type of calculation you would like to perform:
+            1 for addition,
+            2 for subtraction,
+            3 for multiplication,
+            4 for division""")
+
+        firstChoice = float(input('Please enter a number:\n'))
+        secondChoice = float(input('Please enter your next number:\n'))
+
+        if calculation == '1':
+            print('The numbers you entered are {} + {} '.format(firstChoice,secondChoice))
+            print(firstChoice + secondChoice)
+    
+        elif calculation == '':
+                print('The numbers you entered are {} - {} '.format(firstChoice,secondChoice))
+                print(firstChoice - secondChoice)
+
+        elif calculation == '3':
+                print('The numbers you entered are {} * {} '.format(firstChoice,secondChoice))
+                print(firstChoice * secondChoice)
+
+        elif calculation == '4':
+                print('The numbers you entered are {} / {} '.format(firstChoice,secondChoice))
+                print(firstChoice / secondChoice)
+
+        else:
+                print("You have not entered a valid operator, please try again. ")
+
+        go_again()
+
+    def go_again():
+
+        #get user input
+        another_calculation=input("Do you want to make another calculaion?  Enter Y or N ")
+
+        if another_calculation == "Y":
+            calculator()
+
+        elif another_calculation == "y":
+            calculator()
+
+        elif another_calculation == "N":
+            print("Have a nice day!")
+
+        elif another_calculation == "n":
+            print("Have a nice day!")
+
+        else:
+            go_again()
+
+    calculator()
+
+start()
