871e63b62f237b9d57399fc9ef024ea6adbbf3a4
us-alexa2je <alex2je@mail.uc.edu>
2021-11-11 18:51:52 -0500

    node html page2

--- Node/Server.js
+++ Node/Server.js
@@ -2,7 +2,20 @@ var http = require("http");
 
 var server = http.createServer(function(req,res){
     res.writeHead(200, {"Content-Type" : "text/html"});
-    res.end("Hello from Node!")});
+    res.end(`
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Node JS response</title>
+                </head>
+                <body>
+                    <h1>Node JS test!</h1>
+                    <p>${req.url}</p>
+                    <p>${req.method}</p>
+                </body>
+        </html>  
+    
+    `)});
 
     server.listen(3000)
 
