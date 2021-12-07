9df3ec23335734e1e3d58a36343ba3f76d162ec5
us-alexa2je <alex2je@mail.uc.edu>
2021-12-07 03:37:32 -0500

    my Alarm clock app

---
+++ python/Project3/README.md
@@ -0,0 +1,23 @@
+This is a simplified alarm clock program written in python.
+It is designed with a fault text tolerance so the time cannot be incorrectly entered.
+
+```
+if int(timeOfAlarm[0:2]) > 12:
+            return "You have entered and invalid HOUR format! Please try again."
+        elif int(timeOfAlarm[3:5]) > 59:
+            return "You have entered and invalid MINUTE format! Please try again."
+        elif int(timeOfAlarm[6:8]) > 59:
+            return "You have entered an invalid SECOND format! Please try again."
+        else:
+            return "OK. The format is correct."
+```
+It also error checks to be sure of the correct time format, which is 11 digits including the am/pm entry
+
+```
+properTimeLength(timeOfAlarm):
+    if len(timeOfAlarm) != 11:
+        return "Your format is incorrect. Please enter your time in this format: hh:mm:ss am/pm."
+```
+Once the time is entered correctly, the alarm time is ran in a nested loop to compare the actual time and when all entries match up, the message 'Good Morning Sunshine!' appears.  I could not get the wav soundbite to play the music but I did include the path with for those who do have to proper media players able to play it.  This is also preceded with a message stating you alarm will ring in 6 seconds if you have the correct media player installed.
+
+Otherwise, it will throw a code and end.  
---
+++ python/Project3/alarmClock.py
@@ -0,0 +1,63 @@
+# Import the libraries for the time of day and the alarm sound
+from datetime import datetime
+from time import time   
+from playsound import playsound
+
+import time
+
+
+#Validate the user input of the time of day
+def properTimeLength(timeOfAlarm):
+    if len(timeOfAlarm) != 11:
+        return "Your format is incorrect. Please enter your time in this format: hh:mm:ss am/pm."
+    else:
+        if int(timeOfAlarm[0:2]) > 12:
+            return "You have entered and invalid HOUR format! Please try again."
+        elif int(timeOfAlarm[3:5]) > 59:
+            return "You have entered and invalid MINUTE format! Please try again."
+        elif int(timeOfAlarm[6:8]) > 59:
+            return "You have entered an invalid SECOND format! Please try again."
+        else:
+            return "OK. The format is correct."
+            
+
+#Time input being true code
+while True:
+    timeOfAlarm = input("Please enter your alarm time in the following format: HH:MM:SS am/pm.")
+    
+    validate = properTimeLength(timeOfAlarm.lower())
+    if validate != "OK. The format is correct.":
+        print(validate)
+    else:
+        print("You have corrctly enter a time")
+        time.sleep(2)
+        print(f"You alarm is set for {timeOfAlarm}.")
+        break
+
+#Set alarm splices
+alarm_hour = timeOfAlarm[0:2]
+alarm_min = timeOfAlarm[3:5]
+alarm_sec = timeOfAlarm[6:8]
+alarm_period = timeOfAlarm[9:].upper()
+
+#Etablishing the current time
+while True:
+    now = datetime.now()
+
+    present_hour = now.strftime("%I")
+    present_min = now.strftime("%M")
+    present_sec = now.strftime("%S")
+    present_period = now.strftime("%p")
+
+#Compare alarm time to present time 
+    if alarm_period == present_period:
+        if alarm_hour == present_hour:
+            if alarm_min == present_min:
+                if alarm_sec == present_sec:
+                    print("Good Morning Sunshine!")
+                    time.sleep(2)
+                    print("If you have the capabilites to play a wav file, your alarm will sound in 6 seconds, otherwise it will throw a code.")
+                    time.sleep(6)
+
+                    playsound('C:/it3038c-scripts/python/Project3/alarm-deep_groove.wav')
+                    break
